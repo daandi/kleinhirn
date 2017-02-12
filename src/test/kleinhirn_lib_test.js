9d3073f67e62c03f5a668830de5f6c022e7b97d7
Andreas Neumann <andreas@neumann.biz>
2017-02-12 19:05:29 +0100

    zwischenstand

--- src/test/kleinhirn_lib_test.js
+++ src/test/kleinhirn_lib_test.js
@@ -3,4 +3,14 @@ var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
-var FAADataHelper = require('../faa_data_helper');
+var hirn = require('../lib/kleinhirn');
+
+describe('Kleinhirn', function(){
+  var subject = new Kleinhirn();
+  var memoryThingWithNumber = {thing : 'Bier trinken', number: '5'};
+
+  describe('#verbalize', function(){
+    it('returns a list of hotels', function() {
+      var hotels = subject.getPassion(passion).then( function(obj){
+        return obj.body.aggregations.hotels;
+      });
